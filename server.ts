4ca9a3affca342675025fe785c50cac62770d7b5
aryanrsuri <13823329+aryanrsuri@users.noreply.github.com>
2024-11-17 19:29:47 -0800

    rc2
    Signed-off-by: aryanrsuri <13823329+aryanrsuri@users.noreply.github.com>

--- server.ts
+++ server.ts
@@ -1,18 +1,28 @@
 import { serve } from "https://deno.land/std@0.171.0/http/server.ts";
 import { serveFile } from "https://deno.land/std@0.171.0/http/file_server.ts";
 
-const KV = await Deno.openKv();
+const KV = await Deno.openKv(
+  "https://api.deno.com/databases/00ca3a2c-a64c-46a0-8c53-2df142e95b54/connect",
+);
 const PORT = 8000;
+
+interface rfoKey {
+  title: string;
+  status: string;
+  date: string;
+  author: string;
+  amends: string[];
+  is_amended_by: string[];
+}
 
 async function getRfoListItems(): Promise<string> {
   const iterator = KV.list<{ title: string }>({ prefix: ["rfo"] });
   const items = [];
-
   for await (const entry of iterator) {
     const rfoNumber = entry.key[1];
     const { title } = entry.value;
     items.push(
-      `<a href="/rfo/${rfoNumber}">RFO ${rfoNumber}: ${title}</a><br>`,
+      `[<a href="/rfo/${rfoNumber}">${rfoNumber} ${title}</a>]<br>`,
     );
   }
 
@@ -38,25 +48,35 @@ const handler = async (req: Request): Promise<Response> => {
     const rfoNumber = rfoMatch[1];
     try {
       const key = ["rfo", rfoNumber];
-      const meta = await KV.get<
-        { title: string; status: string; date: string }
-      >(key);
+      const meta = await KV.get<rfoKey>(key);
 
       if (!meta.value) {
         return new Response("metadata not found", { status: 404 });
       }
-      const { title, status, date } = meta.value;
+      const { title, status, date, amends, is_amended_by } = meta.value;
+
+      console.log(meta.value);
+
       const essayContent = await Deno.readTextFile(
         `./rfos/${rfoNumber}.htm`,
       );
       const template = await Deno.readTextFile("./src/rfo.html");
+
+      const amends_list = (amends && amends.length > 0)
+        ? amends.map((num) => `<a href="rfo/${num}">${num}"</a>`).join(", ")
+        : "None";
+      const is_amended_by_list = (is_amended_by && is_amended_by.length > 0)
+        ? amends.map((num) => `<a href="rfo/${num}">${num}"</a>`).join(", ")
+        : "None";
 
       const responseHtml = template
         .replace(/{{content}}/g, essayContent)
         .replace(/{{title}}/g, title)
         .replace(/{{rfoNumber}}/g, rfoNumber)
         .replace(/{{date}}/g, date)
-        .replace(/{{status}}/g, status);
+        .replace(/{{status}}/g, status)
+        .replace(/{{amends}}/g, amends_list)
+        .replace(/{{is_amended_by}}/g, is_amended_by_list);
       return new Response(responseHtml, {
         headers: { "Content-Type": "text/html; charset=utf-8" },
       });
@@ -79,24 +99,15 @@ const handler = async (req: Request): Promise<Response> => {
     try {
       // Fetch metadata from Deno KV
       const rfoKey = ["rfo", rfoNumber];
-      const rfoMeta = await KV.get<
-        { title: string; status: string; date: string }
-      >(rfoKey);
+      const rfoMeta = await KV.get<rfoKey>(rfoKey);
 
       if (!rfoMeta.value) {
         return new Response("RFO metadata not found", { status: 404 });
       }
 
       // Convert metadata to plain text
-      const { title, status, date } = rfoMeta.value;
-      const metaText = `
-  RFO Number: ${rfoNumber}
-  Title: ${title}
-  Status: ${status}
-  Date: ${date}`;
-
-      return new Response(metaText, {
-        headers: { "Content-Type": "text/plain; charset=utf-8" },
+      return new Response(JSON.stringify(rfoMeta), {
+        headers: { "Content-Type": "application/json; charset=utf-8" },
       });
     } catch (error) {
       return new Response("Error retrieving metadata", { status: 500 });
